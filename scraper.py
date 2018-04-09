d8bee537d1d635080cde1aa53e556f9846d1e333
Adam Dolaher <adam@Adams-MacBook-Air.local>
2018-04-08 22:56:25 -0400

    Scrape price and address

---
+++ scraper.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+r=requests.get("placeholder")
+c=r.content
+
+soup=BeautifulSoup c, "html.parser"
+
+all=soup.find_all("div",{"class":"propertyRow"})
+
+all[0].find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
+
+for content in all:
+    print(item.find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ",""))
+    print(item.find_all("span",{"class","propAddressCollapse"})[0].text)
+    print(item.find_all("span",{"class","propAddressCollapse"})[1].text)
